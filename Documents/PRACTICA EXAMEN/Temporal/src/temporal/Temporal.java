4514285e6f9394d796d9d9545a4710f7ae20b1e6
Rodrigo Gomez <roodrigo111@gmail.com>
2018-04-12 22:30:12 -0500

    Prueba Java 2

--- Documents/PRACTICA EXAMEN/Temporal/src/temporal/Temporal.java
+++ Documents/PRACTICA EXAMEN/Temporal/src/temporal/Temporal.java
@@ -13,7 +13,8 @@ public class Temporal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Esta es una prueba.");
+        
     }
     
 }
